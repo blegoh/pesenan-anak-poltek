d0b4e14bb6248f8872c7dff73db05efc55417464
blegoh <yusufblegoh@gmail.com>
2017-07-06 20:23:56 +0700

    first commit

---
+++ database/migrations/2017_06_12_120245_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('question');
+            $table->unsignedInteger('if_yes')->nullable();
+            $table->unsignedInteger('if_no')->nullable();
+            $table->boolean('conclution')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
