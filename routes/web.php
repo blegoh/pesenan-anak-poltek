177b68dd317e887230f33bda57841b73e65acab5
blegoh <yusufblegoh@gmail.com>
2017-07-07 15:57:52 +0700

    update web route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin/question/data','Admin\QuestionController@anyData')
+    ->name('question.data');
+Route::resource('/admin/question','Admin\QuestionController');
+
+Route::get('/admin/page/data','Admin\PageController@anyData')
+    ->name('page.data');
+Route::resource('/admin/page','Admin\PageController');
+
+
+Route::get('app','AppController@index');
+Route::get('res','AppController@result');
