6099685e51cccf97396a683a85784bf0c8e216e3
blegoh <yusufblegoh@gmail.com>
2017-07-07 15:57:12 +0700

    update seed

---
+++ database/seeds/PageSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $titles = [
+            'Sejarah', 'Geografi', 'Penduduk', 'Pendidikan',
+            'Etiologi','Epidemiologi','Patofisiologi','Pencegahan dan Pemeriksaan',
+        ];
+
+        foreach ($titles as $title) {
+            \App\Page::create([
+                'slug' => str_slug($title),
+                'title' => $title,
+                'image' => '',
+                'content' => "
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eleifend finibus leo eu bibendum. Donec eu interdum libero. Sed euismod velit magna, vitae suscipit justo maximus eget. Integer vel fringilla eros. Ut in risus ipsum. Pellentesque pharetra rhoncus lacinia. Fusce elementum, nisl id tempus pretium, ligula nibh accumsan leo, in finibus lacus nisl vel felis.
+
+In hac habitasse platea dictumst. Aliquam erat volutpat. Nam vel eros tincidunt, malesuada erat ut, feugiat eros. Nullam sit amet tristique lorem. Donec maximus tristique libero nec feugiat. Vestibulum ac arcu vitae erat dapibus accumsan. Morbi blandit orci id eros tincidunt aliquam. Maecenas et est porttitor, interdum quam eget, gravida nisl. Curabitur vehicula laoreet ultrices. Aliquam sed vehicula massa. Duis semper sagittis mauris tempus bibendum. Mauris at malesuada enim. Ut mi mi, iaculis scelerisque fermentum vitae, commodo et purus. Pellentesque id nibh mattis, consectetur lacus nec, pretium odio. Aenean eu imperdiet dui. Sed molestie lectus nec placerat auctor.
+
+In suscipit neque ut massa volutpat fermentum. Sed vulputate quis nisl in fringilla. Nulla posuere efficitur risus sed dapibus. Sed eget finibus metus. Sed fringilla et turpis vel posuere. Pellentesque convallis purus arcu, id molestie libero luctus ac. Ut ac convallis tortor. Vivamus eget est ullamcorper, viverra leo id, tincidunt nisi. Morbi imperdiet ultricies vestibulum.
+
+Integer at molestie dolor. Fusce sed odio purus. Sed eget gravida augue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi ultricies est vitae rhoncus rutrum. Aenean semper, arcu non elementum tempor, justo metus iaculis ex, nec sollicitudin massa mauris id leo. Phasellus imperdiet ligula vel sagittis dignissim. Aliquam pulvinar tempor erat, non gravida elit mollis vitae. In sagittis risus justo. Nam sagittis porta ante. Praesent vitae vestibulum sem.
+
+Curabitur a nisl sit amet augue rhoncus dictum. Nulla justo odio, rhoncus eu dignissim eu, tincidunt ac orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec vestibulum, nunc at commodo sollicitudin, ex velit malesuada quam, id suscipit ipsum ligula in felis. In nec nisi diam. In sit amet nulla commodo ante congue pellentesque scelerisque eget lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse nunc dui, ultrices eu commodo pellentesque, ornare sit amet nulla. Integer convallis consectetur suscipit. Cras lobortis tempor massa vel tristique. Etiam faucibus nunc eget turpis fringilla volutpat. Vivamus pellentesque, nibh eget maximus sollicitudin, mi eros placerat turpis, in dapibus quam turpis ac justo. ",
+            ]);
+        }
+    }
+}
--- database/seeds/QuestionSeed.php
+++ database/seeds/QuestionSeed.php
@@ -11,6 +11,81 @@ class QuestionSeed extends Seeder
      */
     public function run()
     {
-        //
+        $datas = [
+            [
+                'question' => "Apakah Anda pernah melakukan hubungan seks dibawah usia 20?",
+                'if_yes' => 2,
+                'if_no' => 11,
+            ],
+            [
+                'question' => "Apakah Anda memiliki anak lebih dari 3 ?",
+                'if_yes' => 3,
+                'if_no' => 11,
+            ],
+            [
+                'question' => "Apakah Anda pengguna kontrasepsi oral selama lebih dari 4 tahun?",
+                'if_yes' => 4,
+                'if_no' => 11,
+            ],
+            [
+                'question' => "Apakah siklus menstruasi tidak lancar?",
+                'if_yes' => 5,
+                'if_no' => 11,
+            ],
+            [
+                'question' => "Apakah saat menstruasi terasa sakit berlebih?",
+                'if_yes' => 6,
+                'if_no' => 11,
+            ],
+            [
+                'question' => "Apakah Anda berganti-ganti pasangan seksual?",
+                'if_yes' => 7,
+                'if_no' => 11,
+            ],
+            [
+                'question' => "Apakah terjadi pendarahan setelah menopause?",
+                'if_yes' => 8,
+                'if_no' => 11,
+            ],
+            [
+                'question' => "Apakah terjadi pendarahan diluar menstruasi?",
+                'if_yes' => 9,
+                'if_no' => 11,
+            ],
+            [
+                'question' => "Apakah terdapat benjolan di sekitar area vagina?",
+                'if_yes' => 10,
+                'if_no' => 11,
+            ],
+            [
+                'question' => "Apakah Anda seorang perokok?",
+                'conclusion_yes' => 1,
+                'if_no' => 11,
+            ],
+            [
+                'question' => "Apakah anda menggigil?",
+                'if_yes' => 12,
+                'if_no' => 12,
+            ],
+            [
+                'question' => "Apakah anda mual?",
+                'if_no' => 13,
+                'if_yes' => 13,
+            ],
+            [
+                'question' => "Apakah anda demam?",
+                'if_no' => 14,
+                'if_yes' => 14,
+            ],
+            [
+                'question' => "Apakah perut anda bengkak atau terdapat benjolan?",
+                'conclusion_yes' => 0,
+                'conclusion_no' => 1,
+            ]
+        ];
+
+        foreach ($datas as $data) {
+            \App\Models\Question::create($data);
+        }
     }
 }
