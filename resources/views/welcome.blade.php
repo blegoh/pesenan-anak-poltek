8cd073a8f1794a7e394a2f76d35ed5b5d174cc4b
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2017-09-29 16:18:18 +0700

    update wellcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,127 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Spacial - Index Agency</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <title>Laravel</title>
+    <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+    <link rel="shortcut icon" href="/images/favicon.ico">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- stylesheets -->
+    <link rel="stylesheet" type="text/css" href="scss/bootstrap/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="/dist/theme.min.css" />
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- javascript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="/dist/theme.min.js"></script>
+</head>
+<body>
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
 
-            .full-height {
-                height: 100vh;
-            }
+<!--[if lt IE 8]>
+<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<nav class="navbar navbar-expand-lg navbar-dark bg-transparent" role="navigation">
+    <div class="container no-override">
+        <a class="navbar-brand" href="index.html">
+            <img src="/images/logo-alt-w.png" class="d-none d-lg-inline mr-2 w-25" />
+            CDSS
+        </a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            .position-ref {
-                position: relative;
-            }
+        <div class="collapse navbar-collapse justify-content-end" id="navbar-collapse">
+            <ul class="navbar-nav">
+                <li class="nav-item active dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                        Jember
+                        <i class="ion-chevron-down"></i>
+                    </a>
+                    <div class="dropdown-menu" role="menu">
+                        <a class="dropdown-item" href="/page/sejarah">Sejarah</a>
+                        <a class="dropdown-item" href="/page/geografi">Geografi</a>
+                        <a class="dropdown-item" href="/page/penduduk">Penduduk</a>
+                        <a class="dropdown-item" href="/page/pendidikan">Pendidikan</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                        Kanker Servik
+                        <i class="ion-chevron-down"></i>
+                    </a>
+                    <div class="dropdown-menu" role="menu">
+                        <a class="dropdown-item" href="/page/etiologi">Etiologi</a>
+                        <a class="dropdown-item" href="/page/epidemiologi">Epidemiologi</a>
+                        <a class="dropdown-item" href="/page/patofisiologi">Patofisiologi</a>
+                        <a class="dropdown-item" href="/page/pencegahan-dan-pemeriksaan">Pencegahan dan Pemeriksaan</a>
+                    </div>
+                </li>
+                @if(Auth::check() && Auth::user()->is_admin == 0)
+                <li class="nav-item dropdown dropdown-extend">
+                    <a href="/app" class="nav-link">
+                        Deteksi Dini
+                    </a>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+                </li>
+                @endif
 
-            .content {
-                text-align: center;
-            }
+                @if(Auth::guest())
+                    <li class="nav-item dropdown dropdown-extend">
+                        <a href="/login" class="nav-link">
+                            Login
+                        </a>
 
-            .title {
-                font-size: 84px;
-            }
+                    </li>
+                    <li class="nav-item dropdown dropdown-extend">
+                        <a href="/register" class="nav-link">
+                            Register
+                        </a>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+                    </li>
+                @endif
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+            </ul>
         </div>
-    </body>
+    </div>
+</nav>
+
+<div class="agency-hero">
+    <section class="container">
+        <div class="hero-text">
+            <h1 class="customFadeInUp">
+                Selamat Datang di CDSS
+            </h1>
+            <p class="customFadeInUp">
+                CDSS adalah sistem pakar untuk deteksi dini penyakit kanker servik.
+            </p>
+
+        </div>
+    </section>
+</div>
+
+<script type="text/javascript">
+    $(function () {
+        if (!window.utils.isMobile()) {
+            $(window).scroll(function() {
+                var scroll = $(window).scrollTop(),
+                    slowScroll = scroll/4,
+                    slowBg = 50 + slowScroll;
+
+                $('.agency-hero').css("background-position", "center " + slowBg + "%");
+            });
+
+            utils.parallax_text($(".agency-hero .hero-text"), $(".hero-text").position().top);
+        }
+    });
+</script>
+
+</body>
 </html>
