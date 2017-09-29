f2be12dbdb4032baf3f907ef03e1ab39f9ca03e1
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2017-09-29 20:36:33 +0700

    add logout link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,12 +11,14 @@
     <link rel="shortcut icon" href="/images/favicon.ico">
 
     <!-- stylesheets -->
-    <link rel="stylesheet" type="text/css" href="/scss/bootstrap/bootstrap.css" />
-    <link rel="stylesheet" type="text/css" href="/dist/theme.min.css" />
+    <link rel="stylesheet" type="text/css" href="/scss/bootstrap/bootstrap.css"/>
+    <link rel="stylesheet" type="text/css" href="/dist/theme.min.css"/>
 
     <!-- javascript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+            integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+            crossorigin="anonymous"></script>
     <script src="/dist/theme.min.js"></script>
 </head>
 <body>
@@ -25,13 +27,14 @@
 <![endif]-->
 
 <!--[if lt IE 8]>
-<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
+    your browser</a> to improve your experience.</p>
 <![endif]-->
 
 <nav class="navbar navbar-expand-lg navbar-dark" role="navigation">
     <div class="container no-override">
         <a class="navbar-brand" href="index.html">
-            <img src="/images/logo-alt-w.png" class="d-none d-lg-inline mr-2 w-25" />
+            <img src="/images/logo-alt-w.png" class="d-none d-lg-inline mr-2 w-25"/>
             CDSS
         </a>
         <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse">
@@ -64,12 +67,23 @@
                         <a class="dropdown-item" href="/page/pencegahan-dan-pemeriksaan">Pencegahan dan Pemeriksaan</a>
                     </div>
                 </li>
-                @if(Auth::check() && Auth::user()->is_admin == 0)
-                    <li class="nav-item dropdown dropdown-extend">
-                        <a href="/app" class="nav-link">
-                            Deteksi Dini
-                        </a>
+                @if(Auth::check())
+                    @if(Auth::user()->is_admin == 0)
+                        <li class="nav-item dropdown dropdown-extend">
+                            <a href="/app" class="nav-link">
+                                Deteksi Dini
+                            </a>
 
+                        </li>
+                    @endif
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                            {{Auth::user()->name}}
+                            <i class="ion-chevron-down"></i>
+                        </a>
+                        <div class="dropdown-menu" role="menu">
+                            <a class="dropdown-item" href="/logout">Logout</a>
+                        </div>
                     </li>
                 @endif
 
@@ -96,6 +110,16 @@
 @yield('content')
 
 
-
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+</script>
+@yield('script')
 </body>
 </html>
