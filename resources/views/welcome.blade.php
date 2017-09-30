34462e6bfcd2f6ba8273e99dddcb462468505f98
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2017-09-30 13:17:12 +0700

    update views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,56 +39,7 @@
         </button>
 
         <div class="collapse navbar-collapse justify-content-end" id="navbar-collapse">
-            <ul class="navbar-nav">
-                <li class="nav-item active dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                        Jember
-                        <i class="ion-chevron-down"></i>
-                    </a>
-                    <div class="dropdown-menu" role="menu">
-                        <a class="dropdown-item" href="/page/sejarah">Sejarah</a>
-                        <a class="dropdown-item" href="/page/geografi">Geografi</a>
-                        <a class="dropdown-item" href="/page/penduduk">Penduduk</a>
-                        <a class="dropdown-item" href="/page/pendidikan">Pendidikan</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                        Kanker Servik
-                        <i class="ion-chevron-down"></i>
-                    </a>
-                    <div class="dropdown-menu" role="menu">
-                        <a class="dropdown-item" href="/page/etiologi">Etiologi</a>
-                        <a class="dropdown-item" href="/page/epidemiologi">Epidemiologi</a>
-                        <a class="dropdown-item" href="/page/patofisiologi">Patofisiologi</a>
-                        <a class="dropdown-item" href="/page/pencegahan-dan-pemeriksaan">Pencegahan dan Pemeriksaan</a>
-                    </div>
-                </li>
-                @if(Auth::check() && Auth::user()->is_admin == 0)
-                <li class="nav-item dropdown dropdown-extend">
-                    <a href="/app" class="nav-link">
-                        Deteksi Dini
-                    </a>
-
-                </li>
-                @endif
-
-                @if(Auth::guest())
-                    <li class="nav-item dropdown dropdown-extend">
-                        <a href="/login" class="nav-link">
-                            Login
-                        </a>
-
-                    </li>
-                    <li class="nav-item dropdown dropdown-extend">
-                        <a href="/register" class="nav-link">
-                            Register
-                        </a>
-
-                    </li>
-                @endif
-
-            </ul>
+            @include('layouts.menu')
         </div>
     </div>
 </nav>
