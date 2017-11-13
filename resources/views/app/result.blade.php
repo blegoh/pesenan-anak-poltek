34d4b5b801aef3be4a6a857e5c12e2e08beb6fe6
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2017-11-13 16:35:00 +0700

    update views

--- resources/views/app/result.blade.php
+++ resources/views/app/result.blade.php
@@ -10,6 +10,45 @@
                 </div><!-- end col -->
 
             </div><!-- end row -->
+
+            <br /><br /><br />
+
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-10">
+                    <form class="form-horizontal">
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="email">Kecamatan:</label>
+                            <div class="col-sm-10">
+                                <select name="kecamatan_id" id="" class="form-control">
+                                    @foreach($kecamatans as $kecamatan)
+                                        <option value="{{$kecamatan->id}}">{{$kecamatan->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="pwd">Password:</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <div class="checkbox">
+                                    <label><input type="checkbox"> Remember me</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" class="btn btn-default">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div><!-- end col -->
+
+            </div><!-- end row -->
         </div>
     </div>
 @endsection
