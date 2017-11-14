f534953799b805a626c139dee6b7a732a06f3dac
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2017-11-14 14:32:34 +0700

    update controllers

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -5,12 +5,32 @@ namespace App\Http\Controllers;
 use App\Http\Traits\Resource;
 use App\Models\Record;
 use Illuminate\Http\Request;
+use Datatables;
 
 class RecordController extends Controller
 {
     use Resource;
 
     private $model = Record::class;
-    private $view = 'admin.question';
-    private $route = 'question';
+    private $view = 'admin.records';
+    private $route = 'record';
+
+    public function store(Request $request)
+    {
+        $this->validate($request, Record::$rulesCreate);
+        auth()->user()->update($request->all());
+        auth()->user()->records()->create($request->all());
+        return redirect('/');
+    }
+
+    public function anyData()
+    {
+        return Datatables::of($this->model::query())
+            ->addColumn('action', function ($data) {
+                $del = '<a href="#" data-id="' . $data->id . '" class="btn btn-danger hapus-data">Delete</a>';
+                return $del;
+            })
+            ->make(true);
+    }
+
 }
